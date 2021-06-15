d2155fea3fdc7925b14b36ab5c86122d657ceada
JacoKritzinger <78598184+JacoKritzinger@users.noreply.github.com>
2021-06-15 13:19:36 +0200

    Add files via upload

--- chapter9/graph_tests.py
+++ chapter9/graph_tests.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 
 plt.figure()
+
+plt.xlabel("Categories")
+plt.ylabel("Amounts")
+plt.title("Categories vs. Amounts")
+
 lines = plt.plot(["Men", "Women", "Children"], [3, 5, 9, 25])
 plt.setp(lines, color="#ff5566")
 plt.show()
