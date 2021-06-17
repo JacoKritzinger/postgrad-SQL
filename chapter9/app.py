6cc14ec70e2a0e0a932c0ec853f8d67c49ce7a2b
JacoKritzinger <78598184+JacoKritzinger@users.noreply.github.com>
2021-06-17 12:55:06 +0200

    Add files via upload

--- chapter9/app.py
+++ chapter9/app.py
@@ -2,23 +2,5 @@ import database
 import charts
 import matplotlib.pyplot as plt
 
-MENU_PROMPT = "Enter 'q' to quit, or anything enthing else to chart a new poll."
-POLL_PROMPT = "select the poll id to create a pie chart of the vote percentages."
-
-
-def prompt_select_poll(polls):
-    for poll in polls:
-        print(f"{poll[0]}: {poll[1]}")
-
-    selected_poll = int(input(POLL_PROMPT))
-    _chart_options_for_poll(selected_poll)
-
-def _chart_options_for_poll(poll_id):
-    options = database.get_options(poll_id)
-    figure = charts.create_piea_chart(options)
-    plt.show()
-
-  
-while (user_input :=input(MENU_PROMPT)) != "q":
-    polls = database.get_polls()
-    prompt_select_poll(polls)
+charts.create_bar_chart(database.get_polls_and_votes())
+plt.show
