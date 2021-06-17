ffa6330a03a8bf1bf65ee2729e4fa2769efc87ec
JacoKritzinger <78598184+JacoKritzinger@users.noreply.github.com>
2021-06-17 09:13:17 +0200

    Add files via upload

--- chapter9/graph_tests.py
+++ chapter9/graph_tests.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-figure, (ax1, ax2) = plt.aubplos(1,2)
+figure, (ax1, ax2) = plt.subplos(1,2)
 
 ax1.plot([1, 2, 3, 4], [3, 5, 9, 25])
 ax1.plot([1, 2, 3, 4], [5, 7, 11, 17])
