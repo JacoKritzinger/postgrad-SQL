41274f2b39d2f734c9306cb97fce80b7920e7f80
JacoKritzinger <78598184+JacoKritzinger@users.noreply.github.com>
2021-06-17 09:12:40 +0200

    Add files via upload

--- chapter9/graph_tests.py
+++ chapter9/graph_tests.py
@@ -1,21 +1,9 @@
 import matplotlib.pyplot as plt
 
-figure = plt.figure()
-axes = figure.add_subplot()
-axes.set_title("A test line graph")
-axes.xlabel("Numbers")
-axes.ylabel("Ocxcuences")
-axes.plot([1, 2, 3, 4], [3, 5, 9, 25])
+figure, (ax1, ax2) = plt.aubplos(1,2)
+
+ax1.plot([1, 2, 3, 4], [3, 5, 9, 25])
+ax1.plot([1, 2, 3, 4], [5, 7, 11, 17])
+
 plt.show()
 
-# import matplotlib.pyplot as plt
-
-# plt.figure()
-
-# plt.xlabel("Categories")
-# plt.ylabel("Amounts")
-# plt.title("Categories vs. Amounts")
-
-# lines = plt.plot(["Men", "Women", "Children"], [3, 5, 9, 25])
-# plt.setp(lines, color="#ff5566")
-# plt.show()
