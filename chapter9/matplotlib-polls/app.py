1168d69405ec2a1946a7c4c084528f809c2b2726
JacoKritzinger <78598184+JacoKritzinger@users.noreply.github.com>
2021-06-18 13:36:27 +0200

    Add files via upload

--- chapter9/matplotlib-polls/app.py
+++ chapter9/matplotlib-polls/app.py
@@ -8,7 +8,7 @@ poll_women = [poll[2] for poll in polls]
 
 poll_x_coordinates = range(len(polls))
 
-figure = plt.figure(figsize = (6, 6))
+figure = plt.figure(figsize = (6, 6), linewidth = 5)
 figure.subplots_adjust(bottom = 0.1)
 axes = figure.add_subplot()
 men_plot = axes.bar(
@@ -29,4 +29,4 @@ axes.legend((men_plot,women_plot), ("Men", "Women"))
 
 plt.xticks(poll_x_coordinates, poll_titles, rotation = 30, ha = "right")
 
-figure.savefig("graph.png", bbox_inches="tight", facecolor = "5c44fd")
+figure.savefig("graph.png", bbox_inches="tight", edgecolor = "5c44fd")
