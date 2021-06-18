c9d8d14ef2566ce034700487eeb1d6b8bca893c2
JacoKritzinger <78598184+JacoKritzinger@users.noreply.github.com>
2021-06-18 13:56:42 +0200

    Add files via upload

---
+++ chapter9/3_matplotlib-poll/app.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+figure = plt.figure()
+axes = figure.add_subplot()
+
+
+axes.bar(
+        range(6),
+        [150, 90, 78, 55, 123, 190],
+        tick_label =["Apple", "Burberry", "Google", "Zara", "Microsot", "Superdry"],
+        color=["#5c44fd", "#ff5566", "#5c44fd""#ff5566", "#5c44fd""#ff5566"]
+
+)
+
+plt.xticks(rotation = 30, ha="tight")
+plt.show()
