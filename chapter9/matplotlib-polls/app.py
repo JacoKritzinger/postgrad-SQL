4a901ed26092278f516a6f2933fb74c2b4258f02
JacoKritzinger <78598184+JacoKritzinger@users.noreply.github.com>
2021-06-18 13:31:49 +0200

    Add files via upload

--- chapter9/matplotlib-polls/app.py
+++ chapter9/matplotlib-polls/app.py
@@ -29,4 +29,4 @@ axes.legend((men_plot,women_plot), ("Men", "Women"))
 
 plt.xticks(poll_x_coordinates, poll_titles, rotation = 30, ha = "right")
 
-figure.savefig("graph.png", bbox_inches="tight")
+figure.savefig("graph.png", bbox_inches="tight", pad_inches=2)
