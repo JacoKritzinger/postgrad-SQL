53c65100082e81cceef621ee1fa16b38d225ecba
JacoKritzinger <78598184+JacoKritzinger@users.noreply.github.com>
2021-06-18 13:48:10 +0200

    Add files via upload

--- chapter9/2_matplotlib-poll/app.py
+++ chapter9/2_matplotlib-poll/app.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 figure = plt.figure()
+figure.subplots_adjust(wspace=0.5)
 ax1 = figure.add_subplot(1, 2, 1)
 ax2 = figure.add_subplot(1, 2, 2)
 
@@ -8,5 +9,6 @@ ax1.plot([1, 2, 3, 4], [3, 7, 11, 23])
 ax2.plot([1, 2, 3, 4], [5, 9, 17, 25])
 
 
-plt.show()
-# figure.savefig("graphs.png")
+# plt.show()
+
+figure.savefig("graphs.png", bbox_inches = "tight")
